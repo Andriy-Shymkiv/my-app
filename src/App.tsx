ab9982c9de70dd76f7aa47504aa4385697a0d2ac
Andriy Shymkiv <winamondezik@gmail.com>
2022-11-24 22:12:34 +0200

    game

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
-function App() {
+export const App = () => {
+  const [first, setCountFirst] = useState(0)
+  const [second, setCountSecond] = useState(0)
+  document.body.classList.add('body');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Game">
+        <h2>first player score is <h1>{first}</h1></h2>
+
+        <button
+          className='button'
+          type="button"
+          onClick={() => setCountFirst(current => current + 1)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          plus
+        </button>
+      </div>
+      <div className="Game">
+        <h2>second player score is <h1>{second}</h1></h2>
+
+        <button
+          className='button'
+          type="button"
+          onClick={() => setCountSecond(current => current + 1)}
+        >
+          plus
+        </button>
+      </div>
+      <div className="Game">
+        <h2>minus 10 for all</h2>
+
+        <button
+          className='button'
+          type="button"
+          onClick={() => {
+            setCountFirst(current => current - 10)
+            setCountSecond(current => current - 10)
+          }}
+        >
+          minus
+        </button>
+      </div>
+
+      {first !== 0 || second !== 0
+        ? (
+          <div className="Game">
+            <h2>reset all</h2>
+
+            <button
+              className='button'
+              type="button"
+              onClick={() => {
+                setCountFirst(current => current = 0)
+                setCountSecond(current => current = 0)
+              }}
+            >
+              reset
+            </button>
+          </div>
+        )
+        : null}
+    </div >
   );
 }
 
